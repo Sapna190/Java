b0b8c5d37d8febed1b0162279df1b6ff648dd52e
Sapna190 <sapnasharma8849711@gmail.com>
2025-04-15 10:02:02 +0530

    correct solution

--- recursion/Basics.java
+++ recursion/Basics.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
+
 public class Basics{
 
-    public static int allOccurence(int arr[], int key, int i, int j[]){
-        if(i == arr.length){
-            return -1;
-        }
-        if(arr[i] == key){
-            
-            return j;
-        }
-        return allOccurence(arr, key, i+1,j);
+    public static ArrayList<Integer> allOccurence(int arr[], int key, int i ){
+        ArrayList<Integer> result = new ArrayList<>();
+
+            //Base Case
+            if(i == arr.length){
+            return result;
+            }
+
+
+            //Recusive case
+            if(arr[i] == key){
+            result.add(i);
+            }
+
+            //Recurse for the rest of the array
+            result.addAll(allOccurence(arr, key, i+1));
+
+            return result;
     }
 
     public static void main(String[] args) {
         int arr[] = {3,2,4,5,6,2,7,2,2};
         int key = 2;
-        System.out.println(allOccurence(arr, key, 0, j));
+        System.out.println(allOccurence(arr, key, 0));
     }
 }
