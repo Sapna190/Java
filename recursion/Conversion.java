64675092bc66955d91e705572d47d8b89e5d93ff
Sapna190 <sapnasharma8849711@gmail.com>
2025-04-15 11:20:20 +0530

    correct solution

---
+++ recursion/Conversion.java
@@ -0,0 +1,28 @@
+public class Conversion{
+
+    static String digits[] = {
+         "zero" , "one" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine"
+    };
+
+    public static void printDigit(int number){
+
+        if(number == 0){
+            return;
+        }
+
+        //get the last digit to print it into words
+        int lastDigit = number % 10;
+
+        //recurse the function
+        printDigit(number / 10);
+
+        //print the digit into words
+        System.out.print(digits[lastDigit] + " ");
+    }
+    public static void main(String[] args){
+        int number = 490;
+        printDigit(number);
+        System.out.println();
+
+    }
+}
