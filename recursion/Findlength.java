16849d6e0316b858fbb6b981a7ce7d5f824092c4
Sapna190 <sapnasharma8849711@gmail.com>
2025-04-20 23:41:41 +0530

    correct solution

--- recursion/Findlength.java
+++ recursion/Findlength.java
@@ -9,7 +9,7 @@ public class Findlength {
     }
 
     public static void main(String[] args){
-        String str = "sapn";
+        String str = "sapna";
         System.out.println(length(str));
     }
 }
