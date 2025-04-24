c2f767fec7d81aeb42b7ea0f1e40040fa98a2c91
Sapna190 <sapnasharma8849711@gmail.com>
2025-04-24 23:27:27 +0530

    correct solution

--- JavaScript/func.js
+++ JavaScript/func.js
@@ -5,9 +5,39 @@
 // rollDice();
 
 //create a function to print the multiplication of a table
-function printTable(n){
-    for(let i = n; i <= n*10; i + n){
-        console.log(i);
+// function printTable(n){
+//     for(let i = n; i <= n*10; i + n){
+//         console.log(i);
+//     }
+// }
+// printTable(5);
+
+//let's make a to do app
+let todo = [];
+
+    //make a request
+let req = prompt("Enter your request");
+
+    while(true){
+        if(req == "quit"){
+            console.log("quitting the app");
+            break;
+        }else if(req == "list"){
+            console.log("----------");
+            for(let i=0; i<todo.length; i++){
+                console.log(i, todo[i]);
+            }
+            console.log("----------");
+        }else if(req == "add"){
+            let task = prompt("please add your task");
+            todo.push(task);
+            console.log("task added");
+        }else if(req == "delete"){
+            let idx = prompt("Enter the idx you want to delete");
+            todo.splice(idx,1);
+            console.log("task has been deleted");
+        }else{
+            console.log("you have made the wrong request");
+        }
+         req = prompt("enter you request");
     }
-}
-printTable(5);
