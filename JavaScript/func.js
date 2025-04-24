42882e329b3e3869b72ac80ec43dfec30b664346
Sapna190 <sapnasharma8849711@gmail.com>
2025-04-24 23:36:47 +0530

    correct solution

--- JavaScript/func.js
+++ JavaScript/func.js
@@ -13,31 +13,51 @@
 // printTable(5);
 
 //let's make a to do app
-let todo = [];
+// let todo = [];
 
-    //make a request
-let req = prompt("Enter your request");
+//     //make a request
+// let req = prompt("Enter your request");
 
-    while(true){
-        if(req == "quit"){
-            console.log("quitting the app");
-            break;
-        }else if(req == "list"){
-            console.log("----------");
-            for(let i=0; i<todo.length; i++){
-                console.log(i, todo[i]);
-            }
-            console.log("----------");
-        }else if(req == "add"){
-            let task = prompt("please add your task");
-            todo.push(task);
-            console.log("task added");
-        }else if(req == "delete"){
-            let idx = prompt("Enter the idx you want to delete");
-            todo.splice(idx,1);
-            console.log("task has been deleted");
-        }else{
-            console.log("you have made the wrong request");
-        }
-         req = prompt("enter you request");
-    }
+//     while(true){
+//         if(req == "quit"){
+//             console.log("quitting the app");
+//             break;
+//         }else if(req == "list"){
+//             console.log("----------");
+//             for(let i=0; i<todo.length; i++){
+//                 console.log(i, todo[i]);
+//             }
+//             console.log("----------");
+//         }else if(req == "add"){
+//             let task = prompt("please add your task");
+//             todo.push(task);
+//             console.log("task added");
+//         }else if(req == "delete"){
+//             let idx = prompt("Enter the idx you want to delete");
+//             todo.splice(idx,1);
+//             console.log("task has been deleted");
+//         }else{
+//             console.log("you have made the wrong request");
+//         }
+//          req = prompt("enter you request");
+//     }
+
+
+
+
+//creating object literals
+// const student = {
+//     name : "Sapna Sharma",
+//     enrollment_no : 202203103510081,
+//     Branch : "B tech Computer Science Engineering"
+// };
+
+//Creating a post
+const post = {
+    username : "@Shradha",
+    content : "This is my post",
+    likes : 200,
+    repost : 5,
+    tags : ["@sapna_19" , "@quite.solitaire" ]
+
+};
