17f7c2c3e97f77cf7272c2667910d4895dcae0d8
Sapna190 <sapnasharma8849711@gmail.com>
2025-04-28 23:59:17 +0530

    correct solution

--- JavaScript/JS Dom/app.js
+++ JavaScript/JS Dom/app.js
@@ -3,3 +3,8 @@ let smallImgs = document.getElementsByClassName("oldImg");
 for(let i = 0; i < smallImgs.length; i++){
     console.log(smallImgs[i].src);
 }
+
+
+//manipulating using navigation
+let img = document.querySelector("oldImg");
+console.log(img.parentElement);
