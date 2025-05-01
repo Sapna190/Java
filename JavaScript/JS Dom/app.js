66cd753054be604949b04417ee0fd0ea9bda7d42
Sapna190 <sapnasharma8849711@gmail.com>
2025-05-01 23:45:10 +0530

    correct solution

--- JavaScript/JS Dom/app.js
+++ JavaScript/JS Dom/app.js
@@ -1,11 +1,18 @@
-let smallImgs = document.getElementsByClassName("oldImg");
+// let smallImgs = document.getElementsByClassName("oldImg");
 
-for(let i = 0; i < smallImgs.length; i++){
-    console.log(smallImgs[i].src);
-}
+// for(let i = 0; i < smallImgs.length; i++){
+//     console.log(smallImgs[i].src);
+// }
 
 
-//manipulating using navigation
-let img = document.querySelector("oldImg");
-console.log(img.parentElement);
+// //manipulating using navigation
+// let img = document.querySelector("oldImg");
+// console.log(img.parentElement);
 
+//addEventListener
+let box = document.querySelector('.box');
+box.addEventListener("click", mousee);
+
+function mousee(){
+    console.log("The mouse is entered inside div");
+};
